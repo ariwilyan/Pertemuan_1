9f55351e73151b31f11b234143ef94b56d960b43
PeopCorP <peopcorp@gmail.com>
2019-04-03 21:00:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testapplication/ui/blank/BlankFragment.java
@@ -0,0 +1,36 @@
+package com.example.testapplication.ui.blank;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.testapplication.R;
+
+public class BlankFragment extends Fragment {
+
+    private BlankViewModel mViewModel;
+
+    public static BlankFragment newInstance() {
+        return new BlankFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.blank_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(BlankViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
